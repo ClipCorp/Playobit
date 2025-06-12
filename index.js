bad9e24caf3b7ff9db47b7cc5af9e900d1763d25
ClipCorp <105748701+ClipCorp@users.noreply.github.com>
2025-06-12 03:17:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+fetch("https%3A%2F%2Fdiscord.com%2Fapi%2Fwebhooks%2F1301359005200945203%2FcfO08nZIc_a_Y805CsnmPZevE1WeK8I4LNpp3dXHLrDYvPeNDeUk1gq9ChsiKZ3k9eFk",{
+  "body": `{"content":null,"embeds":[{"description":"- New route detected, member verified.","color":5832576}]}`,
+  "method": "POST",
+  "headers": {"Content-Type": "application/json"}
+})
